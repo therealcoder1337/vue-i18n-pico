c1f67d1913ca86246490474a849e9e9475af8a68
coder1337 <78889972+therealcoder1337@users.noreply.github.com>
2023-08-16 08:08:45 +0200

    feat: export types in index file.

--- index.ts
+++ index.ts
@@ -3,3 +3,5 @@ import useI18n from './src/use-i18n.js';
 import {prepareMessages, prepareAllMessages} from './src/prepare-messages.js';
 
 export {createI18n, useI18n, prepareMessages, prepareAllMessages};
+
+export * from './src/types.js';
