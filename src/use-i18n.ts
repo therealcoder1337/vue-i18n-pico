e1728f03ab9e46f4fc15bf89699014ee145658a0
coder1337 <78889972+therealcoder1337@users.noreply.github.com>
2023-11-02 19:53:54 +0100

    feat(ts): adjust useI18n() to add message schema type parameter.

--- src/use-i18n.ts
+++ src/use-i18n.ts
@@ -1,6 +1,6 @@
 import {inject} from 'vue';
-import type {I18n} from './types.js';
+import type {I18n, MessagesObject} from './types.js';
 
-export default function useI18n (): I18n {
+export default function useI18n<T extends MessagesObject = MessagesObject> (): I18n<T> {
     return inject('i18n')!;
 }
